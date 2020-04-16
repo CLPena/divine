f7106b90eb49116aa0c857e4d3f711b8cf51e8c4
Cristina Pena <cristinalpena@gmail.com>
2020-04-16 10:11:57 -0600

    Add Nav component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.scss';
+import Nav from './Nav/Nav'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <main>
+      <Nav />
+      <div className='random-card-container'>
+      </div>
+      <button className='dashboard-buttons draw-card'>
+        DRAW A CARD
+      </button>
+      <button className='dashboard-buttons browse'>
+        BROWSE CARDS
+      </button>
+    </main>
   );
 }
 
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Nav.scss';
+
+function App() {
+  return (
+    <nav>
+      <button className='home'>
+        MOON IMAGE
+      </button>
+      <div className='nav-text'>
+        <h1>DIVINE</h1>
+        <h3>SIMPLE TAROT</h3>
+      </div>
+    </nav>
+  );
+}
+
+export default App;
