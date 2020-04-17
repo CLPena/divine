1226eec09f3e3d33455206eed63935c2089e7c0f
Cristina Pena <cristinalpena@gmail.com>
2020-04-16 18:42:15 -0600

    Refactor random card to use card component to render

--- src/components/RandomCard/RandomCard.js
+++ src/components/RandomCard/RandomCard.js
@@ -17,21 +17,7 @@ class RandomCard extends Component {
   render() {
     return (
       <div className="dashboard">
-        <div className='card-container'>
-          <div className='card'>
-            <h2>{this.props.card.name}</h2>
-            <img src={goblet} className="goblet" alt="goblet" />
-            <div className="suit-arcana">
-              <h3 >{this.props.card.suit || `trump card`}</h3>
-              <h3>{this.props.card.type}</h3>
-            </div>
-            <h3>meaning:</h3>
-            <p>{this.props.card.meaning_up}</p>
-            <button className="favorite-button">
-              <img src={whiteStar} className="white-star" alt="favorite-card" />
-            </button>
-          </div>
-        </div>
+        <Card name={this.props.card.name} suit={this.props.card.suit} type={this.props.card.type} meaning_up={this.props.card.meaning_up}/>
         <div className='buttons-container'>
           <button className='dashboard-buttons draw-card' onClick={this.props.getRandomCard}>
             <span>DRAW A CARD</span>
@@ -45,8 +31,6 @@ class RandomCard extends Component {
       </div>
     );
   }
-
 }
-
 
 export default RandomCard;
