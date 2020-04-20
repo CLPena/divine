0163ee61a62a372c9d79c937f41a8e42aeea5738
Cristina Pena <cristinalpena@gmail.com>
2020-04-19 18:02:10 -0600

    Finish Nav tests, start tests for RandomCard

--- src/components/Nav/Nav.test.js
+++ src/components/Nav/Nav.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Nav from './Nav';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
-import App from './App';
+import { createStore } from 'redux';
+import { rootReducer } from '../../reducers/'
+import '@testing-library/jest-dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Nav", () => {
+  let testStore;
+  let testWrapper;
+  beforeEach(() => {
+    testStore = createStore(rootReducer);
+    testWrapper = <Provider store={testStore}>
+      <Router>
+        <Nav/>
+      </Router>
+    </Provider>
+  })
+
+  it('should render the logo', () => {
+    const { getByText, getByAltText } = render (testWrapper)
+    const title = getByText("DIVINE");
+    const moon = getByAltText("moon logo")
+    expect(title).toBeInTheDocument();
+    expect(moon).toBeInTheDocument();
+  })
+
+  it('should render with a button to view favorites', () => {
+    const { getByTestId } = render (testWrapper)
+    const favorites = getByTestId("favorites");
+    expect(favorites).toBeInTheDocument();
+  })
+})
