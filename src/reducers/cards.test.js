3bafd1006c6c647002d9205fb8e3c79e753518d6
Cristina Pena <cristinalpena@gmail.com>
2020-04-20 09:46:03 -0600

    Add cards reducer test

---
+++ src/reducers/cards.test.js
@@ -0,0 +1,21 @@
+import { cards } from './cards';
+
+describe('Card Reducers Test', () => {
+  it('should return the initial state', () => {
+    const expected = [];
+    const result = cards(undefined, {});
+    expect(result).toEqual(expected);
+  });
+
+  it('should return cards if type is GET_CARDS', () => {
+    const initialState = [];
+    const expected = [{name: 'First Card'}, {name: 'Second Card'}];
+    const action = {
+      type: 'GET_CARDS',
+      cards: [{name: 'First Card'}, {name: 'Second Card'}]
+    }
+    const result = cards(initialState, action);
+
+    expect(result).toEqual(expected);
+  });
+})
