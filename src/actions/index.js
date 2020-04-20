1762f84eb28ad714cf495fb3928418b97525f877
Cristina Pena <cristinalpena@gmail.com>
2020-04-19 20:47:51 -0600

    Clean up index and apiCalls files

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,24 @@
-export const getCards = (cards)  => ({
-  type: 'GET_CARDS',
-  cards
-})
+export const getCards = (cards) => ({
+  type: "GET_CARDS",
+  cards,
+});
 
 export const getRandomCard = (randomCard) => ({
-  type: 'GET_RANDOM_CARD',
-  randomCard
-})
+  type: "GET_RANDOM_CARD",
+  randomCard,
+});
 
 export const addFavorite = (selectedCard) => ({
-  type: 'ADD_FAVORITE',
-  selectedCard
-})
+  type: "ADD_FAVORITE",
+  selectedCard,
+});
 
 export const deleteFavorite = (selectedCard) => ({
-  type: 'DELETE_FAVORITE',
-  selectedCard
-})
+  type: "DELETE_FAVORITE",
+  selectedCard,
+});
 
 export const search = (results) => ({
-  type: 'SEARCH',
-  results
-})
+  type: "SEARCH",
+  results,
+});
